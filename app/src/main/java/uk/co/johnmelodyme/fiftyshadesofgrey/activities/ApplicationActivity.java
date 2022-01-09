2d90a5029a7507e9eff9372baccbda233638b9b6
John Melody Me <Johnmelodyme@yandex.com>
2022-01-10 01:41:55 +0800

    added list item

--- app/src/main/java/uk/co/johnmelodyme/fiftyshadesofgrey/activities/ApplicationActivity.java
+++ app/src/main/java/uk/co/johnmelodyme/fiftyshadesofgrey/activities/ApplicationActivity.java
@@ -2,18 +2,32 @@ package uk.co.johnmelodyme.fiftyshadesofgrey.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import uk.co.johnmelodyme.fiftyshadesofgrey.R;
 
 public class ApplicationActivity extends AppCompatActivity
 {
     private static final String TAG = "50ShadesOfGrey";
+    public ListView listView;
+
+    public void renderComponents()
+    {
+        /* Set content view to activity */
+        ApplicationActivity.this.getWindow().setContentView(R.layout.activity_main);
+
+        /* declaration of layout user components */
+        listView = (ListView) findViewById(R.id.list_menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        /* Render Components */
+        this.renderComponents();
     }
 }
