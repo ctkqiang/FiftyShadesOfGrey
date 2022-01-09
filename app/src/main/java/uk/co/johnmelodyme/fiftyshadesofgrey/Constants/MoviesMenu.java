4f6bf9c56151b4b80de622c58d4c8a510f22335c
John Melody Me <Johnmelodyme@yandex.com>
2022-01-10 03:30:56 +0800

    Create MoviesMenu.java

---
+++ app/src/main/java/uk/co/johnmelodyme/fiftyshadesofgrey/Constants/MoviesMenu.java
@@ -0,0 +1,19 @@
+package uk.co.johnmelodyme.fiftyshadesofgrey.Constants;
+
+public class MoviesMenu
+{
+    public Integer[] id = {0, 1, 2};
+
+    public String[] name = {
+            "Fifty Shades Of Grey"
+    };
+
+    public String[] url = {
+            "https://firebasestorage.googleapis.com/v0/b/supercuple.appspot"
+            + ".com/o/Fifty-shades%20of%20Grey"
+            + ".MP4?alt=media&token=cc1891da-d676-4dee-b42e-08b80344fb1f",
+            "two",
+            "three"
+    };
+
+}
